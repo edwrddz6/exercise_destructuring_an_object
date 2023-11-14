e27a7ee92a6514c5e88590562b89a351bfadac6e
Edward Diaz <diazedward24@gmail.com>
2023-11-14 11:31:47 -0800

    Add 3 functions

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ How to Submit Assignment:
 Add, commit, and push to your assignments repo. Submit a direct link to the assignment once pushed.
 */
 
+const reverseVowelsAndReplaceL = (string) => {
+    const vowels = 'aeiouAEIOU';
+    const reversedString = string.split('').reverse().join('');
+    const replacedL = reversedString.replace(/L/g, '1');
+
+    console.log(replacedL);
+}
+
+const reverseVowelsAndReplaceO = (string) => {
+    const vowels = 'aeiouAEIOU';
+    const reversedString = string.split('').reverse().join('');
+    const replacedO = reversedString.replace(/O/g, '0');
+
+    console.log(replacedO);
+}
+
+const reverseVowelsAndReplaceS = (string) => {
+    const vowels = 'aeiouAEIOU';
+    const reversedString = string.split('').reverse().join('');
+    const replacedS = reversedString.replace(/S/g, '5');
+
+    console.log(replacedS);
+}
+
+const inputString = "Sally Loves All Kinds of Olives";
+reverseVowelsAndReplaceL(inputString);
+reverseVowelsAndReplaceO(inputString);
+reverseVowelsAndReplaceS(inputString);
