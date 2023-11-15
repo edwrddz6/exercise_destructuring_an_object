c890e9b68c1d206c5b090e153f4de1b24fc68314
Edward Diaz <diazedward24@gmail.com>
2023-11-15 12:20:32 -0800

    Update instructions

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 Write a 3 functions - reverseVowelsAndReplaceL(), reverseVowelsAndReplaceO(), reverseVowelsAndReplaceS(). Apply the following rules:
 
 Each function will receive a string, sometimes provided by other functions
-Each function should reverse the string
-Each function should console.log the reverse string
+Each function should reverse the vowels
+Each function should console.log the reversed vowels
 Use .split(), .join(), .includes() and .replace()
 The input string may contain uppercase and lowercase letters, spaces, and punctuation. You should preserve the position of other characters.
 
@@ -17,9 +17,6 @@ Calls reverseVowelsAndReplaceS() while providing it reverseVowelsAndReplaceO’s
 reverseVowelsAndReplaceS():
 Replaces all 'S's (uppercase and lowercase) with '5's (fives)
 Consoles and returns the final reversed string
-How to Submit Assignment:
-
-Add, commit, and push to your assignments repo. Submit a direct link to the assignment once pushed.
 */
 
 const reverseVowelsAndReplaceL = (string) => {
